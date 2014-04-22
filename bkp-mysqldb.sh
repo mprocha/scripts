f7d257b8ac9f61116007211407df106437cd67ce
mprocha <marcelorocha.unb@gmail.com>
2014-04-21 23:24:24 -0300

    Atualização 2014-04-21 4rd vez

--- bkp-mysqldb.sh
+++ bkp-mysqldb.sh
@@ -4,10 +4,52 @@
 # database server:
 # Marcelo Rocha - UnB - 2014/04/21 - V1.0
 
-date=`date --rfc-3339=date`
+par1=$1
+par2=$2
+
+usage(){
+   echo " "
+   echo "bkp-mysqldb.sh: "
+   echo "Script to generate a backup of database"
+   echo "in a mysql database server."
+   echo " "
+   echo "Marcelo Rocha - UnB - 21-04-2013 - V1.0"
+   echo "email: marcelorocha.unb@gmail.com"
+   echo " "
+   echo "Usage:"
+   echo "bkp-mysqldb.sh [flag] [arg]"
+   echo " "
+   echo "flags:"
+   echo "-h         Show this mensage"
+   echo "-f <file>  Option to choose the output file name"
+   echo "           Default (without arg): backup-yyyy-mm-dd.sql"
+   echo " "
+   echo "The file is gziped and saved in the directory ~/Bkp-mysqldb"
+   echo " "
+} 
+
+if [ $par1 = "-h" ]
+then
+   usage
+   exit 1
+elif [ -z $par1 ]
+then
+   usage
+   exit 1
+elif [ $par1 = "-f" ]
+then
+   if [ -z $par2 ]
+   then   
+      date=`date --rfc-3339=date`
+      file=backup-$date.sql
+   else
+      file=$par2
+   fi
+fi
+
 dir=~/Bkp-mysqldb
 
-echo "  "
+echo " "
 
 if test -d $dir
 then
@@ -17,17 +59,17 @@ else
    echo "Directory "$dir" Created!!!"
 fi
 
-if test -f $dir/backup-$date.sql.gz
+if test -f $dir/$file.gz
 then
-   rm $dir/backup-$date.sql.gz
-   echo $dir/backup-$date.sql.gz" REMOVED!!"
+   rm $dir/$file.gz
+   echo "OLD FILE "$dir"/"$file".gz WAS REMOVED!!"
 fi
 
-echo "  "
+echo " "
 echo "Type mysql user password!!!"
-mysqldump -u root -p -x -e -A > $dir/backup-$date.sql
-gzip $dir/backup-$date.sql
+mysqldump -u root -p -x -e -A > $dir/$file
+gzip $dir/$file
 
 echo "  "
-echo "A new MYSQLDB ("backup-$date.sql.gz") backup was created in dir "$dir
+echo "A new MYSQLDB ("$file".gz) backup was created in dir "$dir
 echo "  "
