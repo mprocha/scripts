0908eab2d7300d605ed129bd06bbf34687620511
suporte <suporte@localhost.localdomain>
2014-05-14 07:44:37 -0300

    Atualização Qua Mai 14 07:44:36 BRT 2014

--- bkpSDSrsync.sh
+++ bkpSDSrsync.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+
 dd=`date +%Y-%m-%d-%H-%m-%S`
 logfile=logfile-$dd.log
+dir=/storage3/Bkp-SDS
 
-> /storage3/Bkp-SDS/$logfile
+> $dir/$logfile
 begin=`date`
-echo "Incio: "$begin  >> $logfile
+echo "Incio: "$begin  >> $dir/$logfile
 
-rsync -av --progress --inplace --log-file="/storage3/Bkp-SDS/rsync.log.$dd" /SDS /storage3/Bkp-SDS
+rsync -av --progress --inplace --log-file="/storage3/Bkp-SDS/rsync.log.$dd" /SDS $dir
 
 end=`date`
-echo "Fim  : "Eend  >> $logfile
+echo "Fim  : "$end  >> $dir/$logfile
