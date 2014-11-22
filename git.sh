f46d02d107380ed62fd8abb24d1339e6824f82b5
mprocha <marcelorocha.unb@gmail.com>
2014-11-22 09:55:40 -0200

    Atualização Sat Nov 22 09:55:08 BRST 2014: "Modificação no script git.sh"

--- git.sh
+++ git.sh
@@ -2,7 +2,10 @@
 
 date=`date`
 
-git init
+read -p "Digite o comentario sobre as modificações feitas: " comment
+
+
+#git init
 git add .
-git commit -m "Atualização $date"
+git commit -m "Atualização $date: $comment"
 git push origin master
