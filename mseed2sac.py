25684587878a66f5c5667d47f61fc2375bfa13a2
mprocha <marcelorocha.unb@gmail.com>
2015-07-15 16:32:14 -0300

    Atualização Qua Jul 15 16:32:04 BRT 2015: alteracoes no mseed2sac.py

--- mseed2sac.py
+++ mseed2sac.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python
 
 from obspy import read
-st=read('AI-BELA-2014.miniseed')
-st.write("test.sac",format="SAC")
+import os, sys
+
+path="/home/marcelo/tmp"
+a=os.listdir( path )
+
+for item in a:
+
+  #st=read('AI-BELA-2014.miniseed')
+  print item
+  st=read(path+"/"+item)
+  st.write("/home/marcelo/"+item+".sac",format="SAC")
+
 quit()
 
